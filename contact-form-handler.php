e39bbdeace4524b0d519a482bd09af5b885a1a1d
Parshant0171 <parshant0171>
2020-06-14 20:40:26 +0530

    contact detail add

---
+++ contact-form-handler.php
@@ -0,0 +1,25 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = 'https://parshant0171.github.io/Proficient';
+
+    $email_subject = "New Client Detail";
+
+    $email_body = "User Name: $name.|\n".
+                        "User Email: $visitor_email.\n".
+                            "User Message: $message.\n";
+
+    $to = "dikshantyadav07@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index.html");
+
+
+?>
